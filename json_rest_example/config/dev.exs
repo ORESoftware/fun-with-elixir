e3c16d602c87fdffe2d222bbcf6575340cd54ad7
bart vemula <bvemula@live.com>
2022-01-13 23:42:31 -0800

    renamed the folder

---
+++ json_rest_example/config/dev.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# For development, we disable any cache and enable
+# debugging and code reloading.
+
+config :elixir_json_restfull_api, :endPoint, port: [port: 4000]
+
+config :elixir_json_restfull_api, :db,
+  # db name
+  database: "User",
+  # pool size
+  pool_size: 3
---
+++ json_rest_example/mix.exs
@@ -0,0 +1,63 @@
+defmodule ElixirJsonRestfullApi.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_json_restfull_api,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :plug_cowboy],
+      mod: {ElixirJsonRestfullApi.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6.1", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.26.0", only: :dev, runtime: false},
+      {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
+      {:excoveralls, "~> 0.14.4", only: :test},
+      {:plug_cowboy, "~> 2.5.2"},
+      {:poison, "~> 5.0.0"},
+      {:mongodb_driver, "~> 0.8.0"},
+      {:poolboy, "~> 1.5.2"}
+    ]
+  end
+
+  def aliases do
+    [
+      test_ci: [
+        "test",
+        "coveralls"
+      ],
+      code_review: [
+        "dialyzer",
+        "credo --strict"
+      ],
+      generate_docs: [
+        "docs",
+        "inch"
+      ]
+    ]
+  end
+end
